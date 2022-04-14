a9b02537ddd9db9158255a8d1adbbeb2b6677d78
Walter Junior <wpmj1277@gmail.com>
2022-04-14 12:14:22 -0300

    Crete User's model

---
+++ api/controllers/UserController.js
@@ -0,0 +1,26 @@
+const User = require("../models/User.js")
+
+class UserController {
+
+    static async listAll(req, res) {
+
+    }
+
+    static async create(req, res) {
+
+    }
+
+    static async getById(req, res) {
+
+    }
+
+    static async update(req, res) {
+
+    }
+
+    static async delete(req, res) {
+
+    }
+}
+
+module.exports = UserController;
---
+++ api/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const usersSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    dob: { type: Date, required: true },
+    address: { type: String, required: true },
+    description: { type: String, required: true },
+    createdAt: { type: Date, required: true }
+});
+
+
+const User = mongoose.model('users', usersSchema);
+
+module.exports = User;
